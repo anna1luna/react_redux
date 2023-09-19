f88a2597c8da887791a4abb27e7f5289a739df33
Anna <anna.pak.vl@gmail.com>
2023-09-19 23:25:24 +0600

    header mod css

--- my-app/src/components/header/header.jsx
+++ my-app/src/components/header/header.jsx
@@ -1,8 +1,10 @@
+import s from './header.module.css'
+
 const Header = () => {
   return (
-    <header className="header bg-info-subtle">
-  <img className="header_logo" src="https://cdn.iconscout.com/icon/free/png-512/free-social-media-3660049-3094483.png?f=avif&w=512" alt="logo"></img>
-  <h1 className="header_heading" > My Social Network </h1>
+    <header className={`${s.header} bg-info-subtle`}>
+  <img className={s.header_logo} src="https://cdn.iconscout.com/icon/free/png-512/free-social-media-3660049-3094483.png?f=avif&w=512" alt="logo"></img>
+  <h1 className={s.header_heading} > My Social Network </h1>
 </header>
   )
 }
