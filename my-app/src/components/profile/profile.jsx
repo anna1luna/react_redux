3431b7dac46c20ff3453b94d49f052b4022be4c0
Anna <anna.pak.vl@gmail.com>
2023-09-24 15:05:06 +0600

    separate component for info

--- my-app/src/components/profile/profile.jsx
+++ my-app/src/components/profile/profile.jsx
@@ -1,37 +1,12 @@
 import s from "./profile.module.css";
 import Post from "./post";
 import News from "./news";
+import Info from "./info";
 
 const Profile = () => {
   return (
     <div className={s.profile}>
-      <div>
-        <img
-          className={s.info_bkgrd}
-          src="https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg"
-          alt="background"
-        ></img>
-      </div>
-      <div className={s.info_data}>
-        <div className={s.info_pic}>
-          <img
-            className={s.info_avatar}
-            src="https://media.istockphoto.com/id/1361956153/photo/black-cat-sticking-out-tongue-funny-portrait.jpg?s=612x612&w=0&k=20&c=JtXn7BNTWl573nyjOJFsI99fXEjctnXsMz5zronzs5A="
-            alt="avatar"
-          ></img>
-        </div>
-        <div>
-          <p className={`${s.info_heading} h2`}>Dmitry K</p>
-          <br></br>
-          <p className={s.info_info}>Date of Birth: 2 january</p>
-          <p className={s.info_info}>City: Minsk </p>
-          <p className={s.info_info}> Education: BSU '11 </p>
-          <p className={s.info_info}>
-            Web Site:{" "}
-            <a href="https://it-kamasutra.com">https://it-kamasutra.com</a>
-          </p>
-        </div>
-      </div>
+      <Info />
       <Post />
       <News text="Hello, World" like_count="20" />
       <News text="My first post" like_count="10" />
