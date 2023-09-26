366bdab38a72a92857494fc102e7d26939effd1c
Anna <anna.pak.vl@gmail.com>
2023-09-26 14:53:26 +0600

    now msgs come frm different side

--- my-app/src/components/msgs/msgs.jsx
+++ my-app/src/components/msgs/msgs.jsx
@@ -3,10 +3,21 @@ import Msg from "./msg";
 
 const Msgs = (props) => {
   const MsgsData = props.MsgsData;
+
   let MsgsReady = MsgsData.map((msg) => (
-    <Msg name={msg.name} text={msg.text} id={msg.id} />
+    <div
+      className={
+        msg.name.includes("Me")
+          ? `${s.msg} ${s.meClass}`
+          : `${s.msg} ${s.otherClass}`
+      }
+      key={msg.id}
+    >
+      <Msg name={msg.name} text={msg.text} id={msg.id} />
+    </div>
   ));
-  return <div className={s.msgs}>{MsgsReady}</div>;
+
+  return <div>{MsgsReady}</div>;
 };
 
 export default Msgs;
