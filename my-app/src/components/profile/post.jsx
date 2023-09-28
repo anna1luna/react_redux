b63e73af1d712aeea9890f3b7fcbb67bf985307f
Anna <anna.pak.vl@gmail.com>
2023-09-28 15:06:53 +0600

    onclick for posts

--- my-app/src/components/profile/post.jsx
+++ my-app/src/components/profile/post.jsx
@@ -1,15 +1,35 @@
-import s from './profile.module.css'
+import React from "react";
+import s from "./profile.module.css";
 
 const Post = () => {
+  let postData = React.createRef();
+  let addPost = () => {
+    let post = postData.current.value;
+    alert(post);
+  };
   return (
     <div className={s.post}>
-            <h2 className={s.post_heading}>My posts</h2>
-            <textarea type="text" placeholder="  what's new?" className={s.post_input}></textarea>
-            <div className={s.post_post}>
-              <button type="button" className={`${s.post_btn} btn btn-success`}> Send</button>
-            </div>
-          </div>
-  )
-}
+      <h2 className={s.post_heading}>My posts</h2>
+      <textarea
+        ref={postData}
+        type="text"
+        placeholder="  what's new?"
+        className={s.post_input}
+      ></textarea>
+      <div className={s.post_post}>
+        <button
+          onClick={() => {
+            addPost();
+          }}
+          type="button"
+          className={`${s.post_btn} btn btn-success`}
+        >
+          {" "}
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
