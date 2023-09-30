f0dd3f9d03c6ea9bfecc3bfa13845d439e6e3196
Anna <anna.pak.vl@gmail.com>
2023-09-30 21:49:10 +0600

    sidebar design

--- my-app/src/components/side/side.jsx
+++ my-app/src/components/side/side.jsx
@@ -5,8 +5,20 @@ import Friends from "./friends";
 const Side = (props) => {
   const SideData = props.SideData;
   let SideReady = SideData.map((friends) => <Friends name={friends.name} />);
+
   return (
-    <nav className={`${s.side} bg-info-subtle`}>
+    <nav className={`${s.side}`}>
+      <div className={s.logo}>
+        <img
+          src="https://media.istockphoto.com/id/1361956153/photo/black-cat-sticking-out-tongue-funny-portrait.jpg?s=612x612&w=0&k=20&c=JtXn7BNTWl573nyjOJFsI99fXEjctnXsMz5zronzs5A="
+          alt="avatar"
+          className={s.avatar_profile}
+        />
+
+        <p className={s.name_profile}>Dmitriy K</p>
+      </div>
+      <hr />
+      <p className={s.menu}>Menu</p>
       <ul className={s.ul}>
         <li className={s.item}>
           {" "}
@@ -16,11 +28,20 @@ const Side = (props) => {
             style={({ isActive, isPending }) => {
               return {
                 fontWeight: isActive ? "bold" : "",
-                color: isPending ? "red" : "black",
+                color: isPending ? "white" : "rgb(111, 127, 146)",
               };
             }}
             to="/profile"
           >
+            <img
+              src="https://www.svgrepo.com/show/512690/profile-1335.svg"
+              alt="profile"
+              style={{
+                marginRight: "8px",
+                width: "20px",
+                height: "20px",
+              }}
+            />
             Profile
           </NavLink>{" "}
         </li>
@@ -31,11 +52,20 @@ const Side = (props) => {
             style={({ isActive, isPending }) => {
               return {
                 fontWeight: isActive ? "bold" : "",
-                color: isPending ? "red" : "black",
+                color: isPending ? "red" : "rgb(111, 127, 146)",
               };
             }}
             to="/messages"
           >
+            <img
+              src="https://www.svgrepo.com/show/533232/message-circle.svg"
+              alt="messages"
+              style={{
+                marginRight: "8px",
+                width: "20px",
+                height: "20px",
+              }}
+            />
             Messages
           </NavLink>{" "}
         </li>
@@ -46,11 +76,20 @@ const Side = (props) => {
             style={({ isActive, isPending }) => {
               return {
                 fontWeight: isActive ? "bold" : "",
-                color: isPending ? "red" : "black",
+                color: isPending ? "red" : "rgb(111, 127, 146)",
               };
             }}
             to="/news"
           >
+            <img
+              src="https://www.svgrepo.com/show/512547/news-grid-1537.svg"
+              alt="news"
+              style={{
+                marginRight: "8px",
+                width: "20px",
+                height: "20px",
+              }}
+            />
             News
           </NavLink>{" "}
         </li>
@@ -61,11 +100,20 @@ const Side = (props) => {
             style={({ isActive, isPending }) => {
               return {
                 fontWeight: isActive ? "bold" : "",
-                color: isPending ? "red" : "black",
+                color: isPending ? "red" : "rgb(111, 127, 146)",
               };
             }}
             to="/music"
           >
+            <img
+              src="https://www.svgrepo.com/show/532708/music.svg"
+              alt="music"
+              style={{
+                marginRight: "8px",
+                width: "20px",
+                height: "20px",
+              }}
+            />
             Music
           </NavLink>{" "}
         </li>
@@ -77,18 +125,40 @@ const Side = (props) => {
             style={({ isActive, isPending }) => {
               return {
                 fontWeight: isActive ? "bold" : "",
-                color: isPending ? "" : "black",
+                color: isPending ? "" : "rgb(111, 127, 146)",
               };
             }}
             to="/settings"
           >
+            <img
+              src="https://www.svgrepo.com/show/522270/settings.svg"
+              alt="settings"
+              style={{
+                marginRight: "8px",
+                width: "20px",
+                height: "20px",
+              }}
+            />
             Settings
           </NavLink>{" "}
         </li>
+
         <br></br>
-        <br></br>
+        <hr />
         <li className={s.item}>
-          <p>Friends</p>
+          <p className={s.friends}>
+            {" "}
+            <img
+              src="https://www.svgrepo.com/show/509977/group.svg"
+              alt="Friends Icon"
+              style={{
+                marginRight: "8px",
+                width: "20px",
+                height: "20px",
+              }}
+            />
+            Friends
+          </p>
           <div className={s.container}>{SideReady}</div>
         </li>
       </ul>
