417b0456a6e835196445a13e40c8c88045b00612
Anna <anna.pak.vl@gmail.com>
2023-10-02 19:42:55 +0600

    likes in red

--- my-app/src/components/profile/news.jsx
+++ my-app/src/components/profile/news.jsx
@@ -16,7 +16,7 @@ const News = (props) => {
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
-              fill="currentColor"
+              fill="red"
               class="bi bi-heart"
               viewBox="0 0 16 16"
             >
