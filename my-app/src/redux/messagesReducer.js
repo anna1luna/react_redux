320dfb467e6354d3e357ff72b95d53854b2f0a71
Anna <anna.pak.vl@gmail.com>
2023-10-18 23:54:39 +0600

    created stateCopy for messagesReducer and SideRedu

--- my-app/src/redux/messagesReducer.js
+++ my-app/src/redux/messagesReducer.js
@@ -1,68 +1,77 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 const UPD_MSG_TEXT = "UPD-MSG-TEXT";
 let initialState = {
-      MsgsData: [
-        {
-          name: "Mama",
-          text: "I am a normal popover and I can have text and everything",
-          id: 1,
-        },
-        {
-          name: "Me",
-          text: "I am a normal popover and I can have text and everything",
-          id: 2,
-        },
-        {
-          name: "Mama",
-          text: "I am a normal popover and I can have text and everything",
-          id: 3,
-        },
-        {
-          name: "Me",
-          text: "I am a normal popover and I can have text and everything",
-          id: 4,
-        },
-        {
-          name: "Mama",
-          text: "I am a normal popover and I can have text and everything",
-          id: 5,
-        },
-      ],
-      msgText: "",
-      SendersData: [
-        { id: 1, name: "Anna" },
-        { id: 2, name: "Mama" },
-        { id: 3, name: "Papa" },
-        { id: 4, name: "Anton" },
-        { id: 5, name: "Anna KL" },
-        { id: 6, name: "Aika" },
-        { id: 7, name: "Yara" },
-        { id: 8, name: "Stas" },
-        { id: 9, name: "Luyda" },
-        { id: 10, name: "Nastya" },
-      ],
-    }
+  MsgsData: [
+    {
+      name: "Mama",
+      text: "I am a normal popover and I can have text and everything",
+      id: 1,
+    },
+    {
+      name: "Me",
+      text: "I am a normal popover and I can have text and everything",
+      id: 2,
+    },
+    {
+      name: "Mama",
+      text: "I am a normal popover and I can have text and everything",
+      id: 3,
+    },
+    {
+      name: "Me",
+      text: "I am a normal popover and I can have text and everything",
+      id: 4,
+    },
+    {
+      name: "Mama",
+      text: "I am a normal popover and I can have text and everything",
+      id: 5,
+    },
+  ],
+  msgText: "",
+  SendersData: [
+    { id: 1, name: "Anna" },
+    { id: 2, name: "Mama" },
+    { id: 3, name: "Papa" },
+    { id: 4, name: "Anton" },
+    { id: 5, name: "Anna KL" },
+    { id: 6, name: "Aika" },
+    { id: 7, name: "Yara" },
+    { id: 8, name: "Stas" },
+    { id: 9, name: "Luyda" },
+    { id: 10, name: "Nastya" },
+  ],
+};
 
 const messagesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_MESSAGE: let newMsg = {
+    case ADD_MESSAGE: {
+      let stateCopy = { ...state };
+      stateCopy.MsgsData = [...state.MsgsData];
+      let newMsg = {
         name: "Me",
-        text: state.msgText,
+        text: stateCopy.msgText,
         id: 6,
       };
       if (newMsg.text.length > 0) {
-        state.MsgsData.push(newMsg);
-        state.msgText = "";
+        stateCopy.MsgsData.push(newMsg);
+        stateCopy.msgText = "";
       } else {
         alert("Please write something :-)");
       }
-      return state
-    case UPD_MSG_TEXT:
-      state.msgText = action.newMsg;
-      return state
-    default: return state
+      return stateCopy;
+    }
+
+    case UPD_MSG_TEXT: {
+      let stateCopy = { ...state };
+      stateCopy.msgText = action.newMsg;
+      return stateCopy;
+    }
+
+    default:
+      return state;
   }
-}
+};
 
 export const addMsgAC = () => {
   return { type: ADD_MESSAGE };
@@ -71,4 +80,4 @@ export const updMsgTextAc = (text) => {
   return { type: UPD_MSG_TEXT, newMsg: text };
 };
 
-export default messagesReducer
+export default messagesReducer;
--- my-app/src/redux/sideReducer.js
+++ my-app/src/redux/sideReducer.js
@@ -1,9 +1,11 @@
 let initialState = {
-      SideData: [{ name: "Anton" }, { name: "Mama" }, { name: "Anna" }],
-    }
+  SideData: [{ name: "Anton" }, { name: "Mama" }, { name: "Anna" }],
+};
 
-const sideReducer = (state =initialState , action) => {
-  return state
-}
+const sideReducer = (state = initialState, action) => {
+  let stateCopy = { ...state };
+  stateCopy.SideData = [...state.SideData];
+  return stateCopy;
+};
 
-export default sideReducer
+export default sideReducer;
